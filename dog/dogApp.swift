eeba21b67358165af38b474467a194b60cb7fc5e
ruameg3 <gevorg.manvelyan@raiffeisen.ru>
2024-10-04 18:46:55 +0300

    initial

---
+++ dog/dogApp.swift
@@ -0,0 +1,17 @@
+//
+//  dogApp.swift
+//  dog
+//
+//  Created by MANVELYAN Gevorg on 04.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct dogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
