e9cd0723a1aa6480e8378bdc34c238aa17896b07
ruameg3 <gevorg.manvelyan@raiffeisen.ru>
2024-10-19 20:38:28 +0300

    tuist

--- Pods/Target Support Files/Pods-dog/Pods-dog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_dog : NSObject
-@end
-@implementation PodsDummy_Pods_dog
-@end
---
+++ Project.swift
@@ -0,0 +1,26 @@
+import ProjectDescription
+
+let target = Target(
+    name: "Dog",
+    platform: .iOS,
+    product: .app,
+    bundleId: "com.asapGev.dog",
+    deploymentTarget: .iOS(targetVersion: "17.4", devices: .iphone),
+    infoPlist: "Info.plist",
+    sources: [.glob( "Dog/**", excluding: "ChatKit/Resources/**")],
+    resources: "Dog/Resources/**",
+    dependencies: [
+        .package(product: "SwiftLint")
+    ]
+)
+
+let project = Project(
+    name: "Dog",
+    packages: [
+            .remote(
+                url: "https://github.com/realm/SwiftLint.git",
+                requirement: .upToNextMajor(from: "0.57.0")
+            )
+        ],
+    targets: [target]
+)
