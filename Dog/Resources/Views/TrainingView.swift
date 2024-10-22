08359a8adea64fb751cd791c3c870a770161fcb6
elliott <elliott>
2024-10-23 00:14:17 +0300

    Added TabBar

---
+++ Dog/Resources/Views/TrainingView.swift
@@ -0,0 +1,18 @@
+//
+//  TrainingView.swift
+//  Dog
+//
+//  Created by Мадина Валиева on 22.10.2024.
+//
+
+import SwiftUI
+
+struct TrainingView: View {
+    var body: some View {
+        Text("Тренировки")
+    }
+}
+
+#Preview {
+    TrainingView()
+}
